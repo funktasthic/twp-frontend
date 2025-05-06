48646d069da1a8cefde1e201eda468798e37a721
funktasthic <ignacio.avendano@alumnos.ucn.cl>
2025-05-06 00:42:54 -0400

    feat(home): create home screen and initial styles

--- src/features/home/screens/HomeScreen.tsx
+++ src/features/home/screens/HomeScreen.tsx
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Image, Text } from 'react-native'
 import React from 'react'
 import HomeStyles from '../styles/home.styles';
+import { PrimaryButton } from '../../../shared/components/button/PrimaryButton';
 
 const HomeScreen = () => {
   return (
     <View style={HomeStyles.container}>
-      <Text>Home screen works!</Text>
+      <View style={HomeStyles.logoContainer}>
+        <Image 
+          source={require("../../../assets/images/twp-logo.png")}
+          style={HomeStyles.logo} />
+        <Image 
+          source={require("../../../assets/images/twp-text-logo-dark.png")}
+          style={HomeStyles.textLogo} />
+      </View>
+      <View style={HomeStyles.buttonGroup}>
+        <PrimaryButton text={"Iniciar sesión"} onPress={() => console.log("Login")} />
+        <PrimaryButton text={"Registrarme"} onPress={() => console.log("Register")} />
+      </View>
     </View>
   )
 }
--- src/features/home/styles/home.styles.ts
+++ src/features/home/styles/home.styles.ts
@@ -4,8 +4,28 @@ import { AppColors } from "../../../shared/themes/AppTheme";
 const HomeStyles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
         backgroundColor: AppColors.background
     },
+    logoContainer: {
+        alignItems: "center"
+    },
+    buttonGroup: {
+        width: "90%",
+        marginBottom: 180,
+        gap: 20
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        top: 40
+    },
+    textLogo: {
+        width: 300,
+        height: 300,
+        top: 20
+    }
 });
 
 export default HomeStyles;
